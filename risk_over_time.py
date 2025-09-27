c38081ee8efe45bb33ccd54d35bb5942d165b417
Eial Kestelman <eial.kestelman@gmail.com>
2025-09-27 19:47:27 -0400

    fix graph labels

--- risk_over_time.py
+++ risk_over_time.py
@@ -8,7 +8,9 @@ st.write('Hello!')
 
 st.write('I wonder if MD style writing works. $1+1=2$.')
 
-years = st.slider('Time', min_value=1, max_value=60, value=10)
+# XXX does this actually work with t=0?
+years = st.number_input(label='Time (truncates to integer)', min_value=0, value=10)
+years = st.slider(label='', min_value=0, max_value=60, value=years)
 
 principal = 1
 
@@ -17,14 +19,17 @@ principal = 1
 # need key for sliders if labels (and all args) are repeated
 mu = st.number_input(label=unicodeit.replace('\mu^*'), value=0., format='%.15g')
 # TODO maybe take out the slider label if we also have the text input?
-mu = st.slider(label='', min_value=-1., max_value=3., value=mu, format='%.15g', key='mu1')
-sigma = st.number_input(label=unicodeit.replace('\sigma^*'), value=0., format='%.15g')
-sigma = st.slider('', min_value=0., max_value=3., value=sigma, format='%.15g', key='sig1')
+# change slider step now that we have reduced the range?
+mu = st.slider(label='', min_value=-0., max_value=.3, value=mu, format='%.15g', key='mu1')
+sigma = st.number_input(label=unicodeit.replace('\sigma^*'), \
+                        min_value=0., value=0., format='%.15g')
+sigma = st.slider('', min_value=0., max_value=.3, value=sigma, format='%.15g', key='sig1')
 
 mu2 = st.number_input(label=unicodeit.replace('\mu^*_2'), value=0., format='%.15g')
-mu2 = st.slider(label='', min_value=-1., max_value=3., value=mu2, format='%.15g', key='mu2')
-sigma2 = st.number_input(label=unicodeit.replace('\sigma^*_2'), value=0., format='%.15g')
-sigma2 = st.slider('', min_value=0., max_value=3., value=sigma2, format='%.15g', key='sig2')
+mu2 = st.slider(label='', min_value=-0., max_value=.3, value=mu2, format='%.15g', key='mu2')
+sigma2 = st.number_input(label=unicodeit.replace('\sigma^*_2'), \
+                         min_value=0., value=0., format='%.15g')
+sigma2 = st.slider('', min_value=0., max_value=.3, value=sigma2, format='%.15g', key='sig2')
 
 benchmark = st.slider('Benchmark Rate', min_value=-1., max_value=3., value=0.)
 
@@ -39,6 +44,7 @@ strat2 = Strat(mu2, sigma2, years, principal)
 st.write('got strats')
 
 compare(strat1.roi_dstr, strat2.roi_dstr, summary=True)
+# FIXME review recalc() func, likely need to run it here
 # have to edit print commands
 st.write('done compare')
 
@@ -55,13 +61,13 @@ st.write('done plot')
 inverse = True
 fig, ax = plt.subplots()
 ax.plot(*strat1.cum_dstr(inverse), label=strat1.label)
-ax.plot(*strat2.cum_dstr(inverse), label=strat1.label)
+ax.plot(*strat2.cum_dstr(inverse), label=strat2.label)
 ax.set_xlabel('Amount')
 if inverse:
-  ax.set_ylabel('P(<x)')
+  ax.set_ylabel('P(>x)')
   ax.set_title('CDF Complement (chance of ending with at least x)')
 else:
-  ax.set_ylabel('P(>x)')
+  ax.set_ylabel('P(<x)')
   ax.set_title('CDF (chance of ending with at most x)')
 ax.vlines(benchmark, 0, 1, color='black', linestyles='--')
 ax.legend()
