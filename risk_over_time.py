18a562cd3af9c0a79ee68e84f522b3a20122794d
Eial Kestelman <eial.kestelman@gmail.com>
2025-09-28 11:01:24 -0400

    fix vertical alignment

--- risk_over_time.py
+++ risk_over_time.py
@@ -12,16 +12,20 @@ mu2 = .15
 sigma2 = .25
 benchmark = .08
 principal = 1.  # type has to match st input type
+input_rows = 7  # nrows = ninputs
 
-st.write('### Welcome to my streamlit app!')
+st.write('# Welcome to my streamlit app :wave:')
 st.write('This is a place to explore my coding projects.')
-st.write('### Investment Risk Over Time')
-st.write('This project is for comparing investments of differing expected returns and variances. Choose the following parameters, and see the results.')
+st.write('## Investment Risk Over Time :chart:')
+st.write('This project is for comparing investments of different expected returns and variances. Choose the following parameters, and see the results.')
 
+# Changing values later does not change this line
 st.write(years, principal, benchmark, mu, sigma, mu2, sigma2)
 
 # border arg only in later versions
 col1, col2 = st.columns([1,2], vertical_alignment='center')
+
+input_grid = [st.columns([1,2]) for _ in range(input_rows)]
 
 # XXX does this actually work with t=0? Yes!
 # if you set a value in num_input, slider changes to reflect that input. But the other
@@ -29,35 +33,43 @@ col1, col2 = st.columns([1,2], vertical_alignment='center')
 # enter on it won't work. Val has to change to update / initiate rerun
 # Better to have slider on left or right? Better to update text on slider or slider on text?
 # FIXME set input to x, set slider to y, set input to z then back to x, val returns to y.
-years = col1.number_input(label='Time (truncates to integer)', min_value=0, value=years)
-years = col2.slider(label='', min_value=0, max_value=60, value=years)
+current_row = 0
+years = input_grid[current_row][0].number_input(label='Time (truncates to integer)', min_value=0, value=years)
+years = input_grid[current_row][1].slider(label='', min_value=0, max_value=60, value=years)
+current_row += 1
 
-principal = col1.number_input(label='Principal', min_value=0., value=principal, step=.1)
+principal = input_grid[current_row][0].number_input(label='Principal', min_value=0., value=principal, step=.1)
 # Makes more sense to give options in thousands?
 # .1 step might represent $100
-principal = col2.slider(label='', min_value=0., max_value=50., value=principal, step=.1)
+principal = input_grid[current_row][1].slider(label='', min_value=0., max_value=50., value=principal, step=.1)
+current_row += 1
 
-benchmark = col1.number_input('Benchmark Rate', value=benchmark, format='%.15g')
-benchmark = col2.slider(label='', min_value=-0., max_value=.5, value=benchmark, format='%.15g')
+benchmark = input_grid[current_row][0].number_input('Benchmark Rate', value=benchmark, format='%.15g')
+benchmark = input_grid[current_row][1].slider(label='', min_value=-0., max_value=.5, value=benchmark, format='%.15g')
+current_row += 1
 
 benchmark = (1 + benchmark) ** years * principal
 
 # format = '%.' + [decimal places] + 'g' for 'general' format
 # can use min_value and max_value to restrict inputs for number_input
 # need key for sliders if labels (and all args) are repeated
-mu = col1.number_input(label=unicodeit.replace('\mu^*'), value=mu, format='%.15g')
+mu = input_grid[current_row][0].number_input(label=unicodeit.replace('\mu^*'), value=mu, format='%.15g')
 # TODO maybe take out the slider label if we also have the text input?
 # change slider step now that we have reduced the range?
-mu = col2.slider(label='', min_value=-0., max_value=.3, value=mu, format='%.15g', key='mu1')
-sigma = col1.number_input(label=unicodeit.replace('\sigma^*'), \
+mu = input_grid[current_row][1].slider(label='', min_value=-0., max_value=.3, value=mu, format='%.15g', key='mu1')
+current_row += 1
+sigma = input_grid[current_row][0].number_input(label=unicodeit.replace('\sigma^*'), \
                         min_value=0., value=sigma, format='%.15g')
-sigma = col2.slider('', min_value=0., max_value=.3, value=sigma, format='%.15g', key='sig1')
+sigma = input_grid[current_row][1].slider('', min_value=0., max_value=.3, value=sigma, format='%.15g', key='sig1')
+current_row += 1
 
-mu2 = col1.number_input(label=unicodeit.replace('\mu^*_2'), value=mu2, format='%.15g')
-mu2 = col2.slider(label='', min_value=-0., max_value=.3, value=mu2, format='%.15g', key='mu2')
-sigma2 = col1.number_input(label=unicodeit.replace('\sigma^*_2'), \
+mu2 = input_grid[current_row][0].number_input(label=unicodeit.replace('\mu^*_2'), value=mu2, format='%.15g')
+mu2 = input_grid[current_row][1].slider(label='', min_value=-0., max_value=.3, value=mu2, format='%.15g', key='mu2')
+current_row += 1
+sigma2 = input_grid[current_row][0].number_input(label=unicodeit.replace('\sigma^*_2'), \
                          min_value=0., value=sigma2, format='%.15g')
-sigma2 = col2.slider('', min_value=0., max_value=.3, value=sigma2, format='%.15g', key='sig2')
+sigma2 = input_grid[current_row][1].slider('', min_value=0., max_value=.3, value=sigma2, format='%.15g', key='sig2')
+current_row += 1
 
 st.write(years, principal, benchmark, mu, sigma, mu2, sigma2)
 
