09a44eb183f90ecf64477bba0b2519e2c833ec6c
Eial Kestelman <eial.kestelman@gmail.com>
2025-10-26 16:27:47 -0400

    expand rot project description

--- landing_page.py
+++ landing_page.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 #st.write('# Welcome to Eial\'s website :wave:')
+#st.write('# Hello there :wave:') # general kenobi easter egg?
 st.write('# Hello, I\'m  Eial!')
 #st.write('My name is Eial. I majored in pure mathematics and minored in astronomy at Stony Brook University. I currently work as a data analyst for the city of New York, and I like to write code for mathematically-oriented projects.')
 st.write('This is a place to explore my coding projects. So far I have only \
@@ -13,6 +14,8 @@ st.write('2. [Wordle Solver](#Wordle-Solver)')
 st.write('## Risk Over Time')
 st.write('In this project, we explore the relationship between investment risk and time \
           scales.')
+st.write('Suppose you are choosing between investment A and investment B. Perhaps A is less volatile, but B has higher potential returns. How do you choose between the two?')
+st.write('Input the mean and standard deviation of each investment, assuming that the returns are lognormally distributed. Then, we can compare the probability of one outperforming the other as a function of time. We can also compare the probability that either investment outperforms some benchmark within a given time scale.')
 st.write('## Wordle Solver')
 st.write('This program tries to solve the wordle puzzle in as few guesses as possible. \
           It takes as input your first guess and the solution, so it won\'t help you \
